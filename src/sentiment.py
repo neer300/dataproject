d73ef0312a01d8f7590957db23e807765a583d24
neer300 <neer300@gmail.com>
2012-12-05 03:41:59 -0500

    commit before pull

--- src/sentiment.py
+++ src/sentiment.py
@@ -4,6 +4,7 @@ from pattern.web import plaintext
 
 import json
 import re
+import tweepy 
 
 from geopy import geocoders
 
@@ -12,6 +13,8 @@ from xml.dom.minidom import parse,parseString
 alchemyObj = AlchemyAPI.AlchemyAPI()
 
 engine = Twitter(license=None,throttle=0.5)
+
+api = tweepy.API(retry_delay=0.5)
 
 # Load the API key from disk.
 alchemyObj.loadAPIKey("../resources/api_key.txt");
@@ -35,72 +38,73 @@ def get_geocode_for_cities(cities):
 
 #cities_name = ["Tehran","New York, NY, USA","Tehran","London, UK","Cairo","Hong Kong","Melbourne VIC, Australia","Sydney NSW, Australia","Seattle, WA, USA", "San Francisco, CA, USA", "Austin, TX, USA", "Los Angeles", "Dublin, Co. Dublin, Ireland", "Leeds, West Yorkshire, UK", "London, UK",  "Mumbai, Maharashtra, India", "New Delhi","Johannesburg, South Africa"]
 
+#cities_name = ["New York, NY, USA"]
 cities_name = ["New York, NY, USA","Seattle, WA, USA", "San Francisco, CA, USA", "Austin, TX, USA", "Los Angeles","Madison, WI, USA","Chicago","Miami","Atlanta","Detroit"]
 #cities = [(40.713956,-74.009399), (37.788081,-122.431641),(47.576526,-122.34375),(30.268556,-97.745361),(34.052659,-118.234863)]
 #cities = get_geocode_for_cities(cities_name)
-cities = [(40.7143528, -74.0059731), (47.6062095, -122.3320708), (37.7749295, -122.4194155), (30.267153, -97.7430608), (34.0522342, -118.2436849), (43.0730517, -89.4012302), (41.8781136, -87.6297982), (25.7889689, -80.2264393), (33.7489954, -84.3879824), (42.331427, -83.0457538)]
+cities = [(40.718119, -74.009399), (47.6062095, -122.3320708), (37.7749295, -122.4194155), (30.267153, -97.7430608), (34.0522342, -118.2436849), (43.0730517, -89.4012302), (41.8781136, -87.6297982), (25.7889689, -80.2264393), (33.7489954, -84.3879824), (42.331427, -83.0457538)]
 
-terms = 'morsi'
+terms = 'gaza'
 
 #city_names=[ "NY", "Seattle", "San Francisco", "Austin", "Los Angeles", "Dublin", "Leeds", "London", "Cairo", "Mumbai", "Delhi","Johannesberg","Melbourne","Sydney","Hong Kong"]
 #cities = [(35.6961111, 51.4230556),(40.713956,-74.009399), (37.788081,-122.431641),(47.576526,-122.34375),(30.268556,-97.745361),(34.052659,-118.234863),(53.347272,-6.262207),(53.807139,-1.549072),(51.508742,-0.12085),(30.107118,31.201172),(19.077693,72.877808),(28.637568,77.228394),(-26.194877,28.081055),(-37.788081,145.019531),(-33.861293,151.204834),(22.319589,114.21936)]
-fout = open("../resources/tweets_morsi.txt",'w')
-fout2 = open("../resources/results_morsi.txt",'w')
+fout = open("../resources/tweets_gaza2.txt",'w')
+fout2 = open("../resources/results_gaza2.txt",'w')
 
 city_scores= []
 for i in range(len(cities)):
-    scores = []
+    fout = open("../resources/tweets_gaza_"+cities_name[i]+".txt",'w')
+#    scores = []
     geo_code  = cities[i]
-    json_obj = {}
-    json_obj["geocode"] = geo_code
-    json_obj["terms"] = terms; 
-    fout.write(cities_name[i] + ":\n"); 
-    for result in engine.search(terms,count = 500,start = i,geo=geo_code):
-        tweet  = plaintext(result.description).encode('utf-8')
-        try:
-            result = alchemyObj.TextGetTextSentiment(tweet);
-            
-            dom = parseString(result);
-            reflist = dom.getElementsByTagName('score');
-            if len(reflist) > 0:
-                score = float(reflist[0].firstChild.data);
-            else:
-                score = 0.0;
-                
-            search = re.search("(?P<url>https?://[^\s]+)", tweet)
-            if search != None:
-                res = search.group("url")
-                result = alchemyObj.URLGetTextSentiment(res);
-                dom = parseString(result);
-                reflist = dom.getElementsByTagName('score');
-                
-                if len(reflist) > 0:
-                    score += float(reflist[0].firstChild.data);
-                else:
-                    score += 0.0;
-                    
-            scores.append(score)
-            print tweet + ":" + str(score)
-            fout.write(tweet + " : " + str(score) + "\n")
-            
-                
-        except:
-            print "error"
-            
-    city_scores.append(scores)
-    json_obj["scores"] = scores
-    json_encoded = json.dumps(json_obj)
-    print json_encoded
-    fout2.write(json_encoded+ "\n")
+    g = str(geo_code[0])+',' + str(geo_code[1]) + ',10mi'
     
-#print city_scores
-#i=0
-#for scores in city_scores:
-#    fout2.write("\n"+cities_name[i])
-#    for score in scores:
-#        fout2.write(score + " ");
-#    
-#    i = i+1
+#    json_obj = {}
+#    json_obj["geocode"] = geo_code
+#    json_obj["terms"] = terms; 
+#    fout.write(cities_name[i] + ":\n"); 
+    for p in range(1,31):
+        for result in  api.search(q=terms,geocode=g,page=p):
+            tweet  = plaintext(result.text).encode('utf-8')
+            fout.write(tweet+"\n")
+    
+    fout.close();
+#            try:
+#                result = alchemyObj.TextGetTextSentiment(tweet);
+#                
+#                dom = parseString(result);
+#                reflist = dom.getElementsByTagName('score');
+#                if len(reflist) > 0:
+#                    score = float(reflist[0].firstChild.data);
+#                else:
+#                    score = 0.0;
+                    
+#                search = re.search("(?P<url>https?://[^\s]+)", tweet)
+#                if search != None:
+#                    res = search.group("url")
+#                    result = alchemyObj.URLGetTextSentiment(res);
+#                    dom = parseString(result);
+#                    reflist = dom.getElementsByTagName('score');
+#                    
+#                    if len(reflist) > 0:
+#                        score += float(reflist[0].firstChild.data);
+#                    else:
+#                        score += 0.0;
+#                        
+#                scores.append(score)
+#                print tweet + ":" + str(score)
+#                fout.write(tweet + " : " + str(score) + "\n")
+#                
+#                    
+#            except:
+#                print "error"
+#                fout.write(tweet+ "\n")
+            
+#    city_scores.append(scores)
+#    json_obj["scores"] = scores
+#    json_encoded = json.dumps(json_obj)
+#    print json_encoded
+#    fout2.write(json_encoded+ "\n")
+    
 
 fout.close()
 fout2.close()
--- src/tweetdump.py
+++ src/tweetdump.py
@@ -1,11 +1,13 @@
 import tweepy
 
 from geopy import geocoders
+import time
 
 def get_geocode_for_cities(cities):
     cities_geo = []
     for city in cities:
         georet = geoeng.geocode(city, exactly_one=False)
+        time.sleep(0.5)
         # geocode has different kinds of return values, list of tuples or a tuple.
         if type(georet) is type([]):
             place, (lat, lng) = georet[0]
@@ -21,8 +23,14 @@ geoeng = geocoders.Google()
 cities_name = ["New York, NY, USA","Seattle, WA, USA", "San Francisco, CA, USA", "Austin, TX, USA", "Los Angeles","Madison, WI, USA","Chicago","Miami","Atlanta","Detroit"]
 cities = [(40.7143528, -74.0059731), (47.6062095, -122.3320708), (37.7749295, -122.4194155), (30.267153, -97.7430608), (34.0522342, -118.2436849), (43.0730517, -89.4012302), (41.8781136, -87.6297982), (25.7889689, -80.2264393), (33.7489954, -84.3879824), (42.331427, -83.0457538)]
 
+cities_name = ["Cairo","Tehran","Dubai","Jerusalem","Riyadh","Karachi","Baghdad","Istanbul"]
+cities = [(30.0444196, 31.2357116), (35.6961111, 51.4230556), (25.271139, 55.307485), (31.768319, 35.21371), (24.7116667, 46.7241667), (24.893379, 67.0280609), (33.325, 44.422), (41.00527, 28.97696)]
 
-terms = 'obama health'
+cities_name += ["London","Hong Kong","Tokyo","Mumbai","Sydney","Beijing","Dublin","Paris","Rome","Madrid","Berlin","Moscow"]
+cities += [(51.5073346, -0.1276831), (22.396428, 114.109497), (35.6894875, 139.6917064), (19.0759837, 72.8776559), (-33.8674869, 151.2069902), (39.90403, 116.407526), (53.3494426, -6.2600825), (48.856614, 2.3522219), (41.9015141, 12.4607737), (40.4167754, -3.7037902), (52.519171, 13.4060912), (55.7512419, 37.6184217)]
+#cities = get_geocode_for_cities(cities_name)
+
+terms = 'gaza'
 for i in range(len(cities)):
     fout = open("../resources/tweets_"+terms+"_"+cities_name[i]+".txt",'w')
     fout.write(cities_name[i]+"\n")
@@ -32,7 +40,7 @@ for i in range(len(cities)):
     g = str(geo_code[0])+',' + str(geo_code[1]) + ',10mi'
     print "for" + cities_name[i] + ":"
     for p in range(1,8):
-        for result in  api.search(q=terms,geocode=g,page=p):
+        for result in  api.search(q=terms,geocode=g,page=p,lang='en'):
             tweet  = result.text
             print tweet
             fout.write(tweet+"\n")
